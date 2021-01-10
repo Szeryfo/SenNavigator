87da5156cbd478aa3928c5f196194db1da2f9ca0
Szeryf <sebastiankurzel@gmail.com>
2021-01-10 12:19:09 +0100

    Added SettingsActivity

--- app/src/main/java/com/example/sennavigator/MainActivity.java
+++ app/src/main/java/com/example/sennavigator/MainActivity.java
@@ -6,13 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
 public class MainActivity extends FragmentActivity {
 
 
@@ -27,6 +20,12 @@ public class MainActivity extends FragmentActivity {
         Button btnMap = findViewById(R.id.btnMap);
         btnMap.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, MapActivity.class);
+            startActivity(intent);
+        });
+
+        Button btnSettings = findViewById(R.id.btnSettings);
+        btnSettings.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
             startActivity(intent);
         });
     }
